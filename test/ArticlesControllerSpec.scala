67e797c0f16f88ad71ebd103c2d8f65462f176b7
Matt Seburn <matthew.seburn@cbc.ca>
2016-02-04 20:35:26 -0500

    refactor articles spec

---
+++ test/ArticlesControllerSpec.scala
@@ -0,0 +1,24 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+@RunWith(classOf[JUnitRunner])
+class ArticlesSpec extends Specification {
+  lazy private val headers = "Authorization" -> play.Play.application.configuration.getString("authentication.key")
+
+  "Articles" should {
+
+    "require authentication" in new WithApplication{
+      route(FakeRequest(GET, "/articles")) must beSome.which (status(_) == UNAUTHORIZED)
+      route(FakeRequest(GET, "/articles").withHeaders(headers)) must beSome.which (status(_) == OK)
+    }
+  }
+}
